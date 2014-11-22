a1a1eeae897f07f4e2779e7f2b3a4196ff01f672
Terry M. Poulin <BigBoss1964@gmail.com>
2014-11-22 12:18:22 -0500

    Throw up a uber basic web view.

--- android/src/main/java/com/spidey01/morder/android/BrowserActivity.java
+++ android/src/main/java/com/spidey01/morder/android/BrowserActivity.java
@@ -20,6 +20,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+
 
 public class BrowserActivity extends Activity {
 
@@ -27,5 +29,11 @@ public class BrowserActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.browser_activity);
+
+        WebView view = (WebView) findViewById(R.id.webview);
+        view.setWebViewClient(new MorderWebViewClient());
+        if (view != null) {
+            view.loadUrl("http://www.google.com");
+        }
     }
 }
---
+++ android/src/main/java/com/spidey01/morder/android/MorderWebViewClient.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2014 Terry Mathew Poulin <BigBoss1964@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.spidey01.morder.android;
+
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+/** Custom WebViewClient to load content in our own web view.
+ *
+ * Created by terry on 11/22/2014.
+ */
+public class MorderWebViewClient
+    extends WebViewClient
+{
+    /**
+     * Give the host application a chance to take over the control when a new
+     * url is about to be loaded in the current WebView. If WebViewClient is not
+     * provided, by default WebView will ask Activity Manager to choose the
+     * proper handler for the url. If WebViewClient is provided, return true
+     * means the host application handles the url, while return false means the
+     * current WebView handles the url.
+     * This method is not called for requests using the POST "method".
+     *
+     * @param view The WebView that is initiating the callback.
+     * @param url  The url to be loaded.
+     * @return True if the host application wants to leave the current WebView
+     * and handle the url itself, otherwise return false.
+     */
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        //return super.shouldOverrideUrlLoading(view, url);
+        return false;
+    }
+}
