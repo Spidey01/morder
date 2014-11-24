6a38746763b4790bcf60371c299f2927143ed7ab
Terry M. Poulin <BigBoss1964@gmail.com>
2014-11-23 22:57:28 -0500

    Back+backspace now step back through history.

--- android/src/main/java/com/spidey01/morder/android/browser/BrowserActivity.java
+++ android/src/main/java/com/spidey01/morder/android/browser/BrowserActivity.java
@@ -84,6 +84,48 @@ public class BrowserActivity
     }
 
 
+    /**
+     * Called when a key was pressed down and not handled by any of the views
+     * inside of the activity. So, for example, key presses while the cursor
+     * is inside a TextView will not trigger the event (unless it is a navigation
+     * to another object) because TextView handles its own key presses.
+     * <p/>
+     * <p>If the focused view didn't want this event, this method is called.
+     * <p/>
+     * <p>The default implementation takes care of {@link android.view.KeyEvent#KEYCODE_BACK}
+     * by calling {@link #onBackPressed()}, though the behavior varies based
+     * on the application compatibility mode: for
+     * {@link android.os.Build.VERSION_CODES#ECLAIR} or later applications,
+     * it will set up the dispatch to call {@link #onKeyUp} where the action
+     * will be performed; for earlier applications, it will perform the
+     * action immediately in on-down, as those versions of the platform
+     * behaved.
+     * <p/>
+     * <p>Other additional default key handling may be performed
+     * if configured with {@link #setDefaultKeyMode}.
+     *
+     * @param keyCode
+     * @param event
+     * @return Return <code>true</code> to prevent this event from being propagated
+     * further, or <code>false</code> to indicate that you have not handled
+     * this event and it should continue to be propagated.
+     * @see #onKeyUp
+     * @see android.view.KeyEvent
+     */
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Note: Android calls backspace DEL and delete FORWARD_DEL.
+        if ((keyCode == KeyEvent.KEYCODE_BACK || keyCode == KeyEvent.KEYCODE_DEL)
+            && mWebView.canGoBack())
+        {
+            mWebView.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
+
+
 }
 
 
